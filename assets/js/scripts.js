417a989c065bc9e6d598e77cc2ce2f1272278ef8
Marcos Alves <mrcaurelio95@gmail.com>
2025-05-08 21:52:13 -0300

    refactor: no estilo do menu e mudança na cor enfase

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,3 +2,13 @@ function showMenu() {
   const navigation = document.getElementById("navigation");
   navigation.classList.toggle("show");
 }
+
+window.addEventListener("scroll", () => {
+  const header = document.querySelector("header");
+
+  if (window.scrollY > 50) {
+    header.classList.add("transparent");
+  } else {
+    header.classList.remove("transparent");
+  }
+});
